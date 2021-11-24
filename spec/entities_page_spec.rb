ab178fc3ff5eba23c5a78c4832ce6dd613c19542
Sebastián M <smunozmo@gmail.com>
2021-11-24 09:28:28 -0500

    Add total amount to transactions page.

--- spec/entities_page_spec.rb
+++ spec/entities_page_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe 'In transactions page', js: true, type: :system do
   before(:all) do
     user1 = User.create({ name: 'Foo', email: 'user1@mail.com', password: '111111'})
-    visit new_user_session_path
-    fill_in 'user_email', with: 'user1@mail.com'
-    fill_in 'user_password', with: '111111'
-    click_button 'Log in'
     group1 = Group.create({name: 'Transport', icon: 'https://lorempixel.com/100/100/transport/', user_id: user1.id})
     entity1 = Entity.create({name: '001 Transaction', amount: 10, user_id: user1.id})
     group_entity1 = GroupEntity.create({entity_id: entity1.id, group_id: group1.id})
